d0dc082d9be6b864ec63740d22c314d543b9a5aa
Reshmi3132 <70102871+Reshmi3132@users.noreply.github.com>
2020-08-31 14:15:17 +0530

    Update my-telegram-appbot.py

--- my-telegram-appbot.py
+++ my-telegram-appbot.py
@@ -11,21 +11,29 @@ from Adafruit_IO import Client, Feed
 aio = Client(x,y)
 
 bot = telebot.TeleBot(z)
+
+from telegram.ext import Updater
+import requests
+chat_id ='1112488868'
+
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-    bot.reply_to(message, "Hi, Tell your command")
+    bot.reply_to(message, "Hi, Would you like to turn on or off the light?(Type 'Light on' for on and 'Light off' for off)")
+    bot.send_photo(chat_id, photo=("https://img.freepik.com/free-vector/flat-light-bulbs-turned-turned-off-with-light-switches_44703-329.jpg?size=626&ext=jpg"))
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
-  if str(message.text) == 'Light on':
+  if str(message.text) == 'Light on' or str(message.text) == 'light on' or str(message.text) == 'On' or str(message.text) == 'on':
     from Adafruit_IO import Data
     data = Data(value=1)
     aio.create_data('mylightbot', data)
     bot.reply_to(message,'light is on')
-  elif str(message.text) =='Light off':
+    bot.send_photo(chat_id, photo=("https://ak.picdn.net/shutterstock/videos/3560576/thumb/1.jpg"))
+  elif str(message.text) == 'Light off' or str(message.text) == 'light off' or str(message.text) == 'Off' or str(message.text) == 'off':
     from Adafruit_IO import Data
     data = Data(value=0)
     aio.create_data('mylightbot', data)
     bot.reply_to(message,'light is off')
+    bot.send_photo(chat_id, photo=("https://ak.picdn.net/shutterstock/videos/3560594/thumb/1.jpg"))
 bot.polling()
- 
+
